2d7ae5c5e99945d4d00763859a7a84d453bb1de3
Felipe Robledo <felipe.robledo@prezi.com>
2021-07-20 21:30:14 -0400

    feat: generate compile_commands.json with cmake
    This helps ccls to understand where to find include files for a complete autocompletion engine within a c++ project.
    The only downside with this approach is that the compile_commands.json has paths for the specific
    build target.

--- src/console_target/build.sh
+++ src/console_target/build.sh
@@ -11,8 +11,15 @@ pushd build
     # Request that CMake configure itself based on what it finds in the parent folder.
     echo "Configuring CMake with Ninja ..."
     cmake -G Ninja ..
-	
+
     # Start the build process.
     echo "Building project with Ninja ..."
     ninja
+
+    # Copying compile_commands.json to help editor use info to find project inclues
+    if [[ -f "compile_commands.json" ]];then
+        cp compile_commands.json ../../../compile_commands.json
+    else
+        echo "WARNING: compile_commands.json not found"
+    fi
 popd
